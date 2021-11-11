874d454f8c0339b2be5e5b64a4a42149de2befa5
Jérémie Piellard <jeremie.piellard@gmail.com>
2021-11-11 17:56:20 +0100

    Update dependencies

--- script/page.js
+++ script/page.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 var Page;
 (function (Page) {
     var Homepage;
@@ -37,13 +36,13 @@ var Page;
             /**
              * Updates the logo's href with an url from the shuffled list.
              */
-            function randomizeLogoHref() {
+            var randomizeLogoHref = function () {
                 if (nextIndex_1 === 0) {
                     shuffleList(projectsUrls_1);
                 }
                 logo.href = projectsUrls_1[nextIndex_1];
                 nextIndex_1 = (nextIndex_1 + 1) % projectsUrls_1.length;
-            }
+            };
             logo.classList.add("dynamic-logo");
             logo.href = "#"; // default value if no projects on the page
             logo.onclick = randomizeLogoHref;
