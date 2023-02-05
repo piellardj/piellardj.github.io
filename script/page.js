4dca2e839c442f293151ec66f383a24e5516ff90
Jérémie Piellard <jeremie.piellard@gmail.com>
2023-02-05 16:41:48 +0100

    iOS :hover fix 3

--- script/page.js
+++ script/page.js
@@ -56,4 +56,43 @@ var Page;
     })(Homepage = Page.Homepage || (Page.Homepage = {}));
 })(Page || (Page = {}));
 
+var Page;
+(function (Page) {
+    var Homepage;
+    (function (Homepage) {
+        var Card;
+        (function (Card) {
+            function bindCardsActiveEvent() {
+                var allCardFrames = document.querySelectorAll(".card-frame");
+                var ACTIVE_CLASS = "active";
+                document.addEventListener("click", function (event) {
+                    for (var i = 0; i < allCardFrames.length; i++) {
+                        var cardFrame = allCardFrames[i];
+                        cardFrame.classList.remove(ACTIVE_CLASS);
+                    }
+                    var target = event.target;
+                    var current = target;
+                    while (current) {
+                        if (current.classList.contains("card-frame")) {
+                            current.classList.add(ACTIVE_CLASS);
+                            break;
+                        }
+                        current = current.parentElement;
+                    }
+                });
+                var _loop_1 = function (i) {
+                    var cardFrame = allCardFrames[i];
+                    cardFrame.addEventListener("mouseleave", function () {
+                        cardFrame.classList.remove(ACTIVE_CLASS);
+                    });
+                };
+                for (var i = 0; i < allCardFrames.length; i++) {
+                    _loop_1(i);
+                }
+            }
+            bindCardsActiveEvent();
+        })(Card = Homepage.Card || (Homepage.Card = {}));
+    })(Homepage = Page.Homepage || (Page.Homepage = {}));
+})(Page || (Page = {}));
+
 
