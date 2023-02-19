df723bde30075b2a7e54cdd8713891acd7000ae7
Jérémie Piellard <jeremie.piellard@gmail.com>
2023-02-19 10:02:54 +0100

    Ad back "particles-webgpu"
    which was removed by mistake

--- src/generate-page.ts
+++ src/generate-page.ts
@@ -19,6 +19,19 @@ const data = {
             title: "Web",
             cards: [
                 {
+                    background: "images/projects/particles-webgpu_512.jpg",
+                    background_light: "images/projects/particles-webgpu_512.jpg",
+                    background_light_highdpi: "images/projects/particles-webgpu_1024.jpg",
+                    title: "Particles (WebGPU)",
+                    body: [
+                        "This is a basic particles simulation running fully on GPU, using the new WebGPU API.",
+                        "Particles evolve independently, following simple gravitational rules. There can be several attraction points at once. You can control one with your mouse by pressing the left mouse button.",
+                    ],
+                    projectName: "particles-webgpu",
+                    liveLink: true,
+                    liveLinkArguments: "page%3Acanvas%3Afullscreen=true&page%3Acanvas%3Asidepane=true&page%3Arange%3Aparticles-count-range-id=3"
+                },
+                {
                     background: "images/projects/tessellation-webgl_512.jpg",
                     background_blurred: "images/projects/tessellation-webgl_blurred.png",
                     background_light: "images/projects/tessellation-webgl_512.webp",
