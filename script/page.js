9122d796507ec1d2e78fd1a913ec35a04b70e8d3
Jérémie Piellard <jeremie.piellard@gmail.com>
2023-04-24 23:16:36 +0200

    Mobile: activate card on title click

--- script/page.js
+++ script/page.js
@@ -63,9 +63,11 @@ var Page;
         var Card;
         (function (Card) {
             function bindCardsActiveEvent() {
+                var _a;
                 var allCardFrames = document.querySelectorAll(".card-frame");
                 var ACTIVE_CLASS = "active";
                 document.addEventListener("click", function (event) {
+                    var _a;
                     for (var i = 0; i < allCardFrames.length; i++) {
                         var cardFrame = allCardFrames[i];
                         cardFrame.classList.remove(ACTIVE_CLASS);
@@ -77,12 +79,18 @@ var Page;
                             current.classList.add(ACTIVE_CLASS);
                             break;
                         }
+                        else if (current.classList.contains("card-title")) {
+                            (_a = current === null || current === void 0 ? void 0 : current.nextElementSibling) === null || _a === void 0 ? void 0 : _a.classList.add(ACTIVE_CLASS);
+                        }
                         current = current.parentElement;
                     }
                 });
                 var _loop_1 = function (i) {
                     var cardFrame = allCardFrames[i];
                     cardFrame.addEventListener("mouseleave", function () {
+                        cardFrame.classList.remove(ACTIVE_CLASS);
+                    });
+                    (_a = cardFrame.parentElement) === null || _a === void 0 ? void 0 : _a.addEventListener("mouseleave", function () {
                         cardFrame.classList.remove(ACTIVE_CLASS);
                     });
                 };
